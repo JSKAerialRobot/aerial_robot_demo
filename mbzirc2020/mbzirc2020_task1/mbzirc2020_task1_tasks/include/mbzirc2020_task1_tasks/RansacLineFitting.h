0668010a07e07c8f8c05f6bc3a459c68b39a9aa3
KahnShi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2020-01-09 20:55:33 +0900

    Add RANSAC for 3D line estimation.

--- mbzirc2020/mbzirc2020_task1/mbzirc2020_task1_tasks/include/mbzirc2020_task1_tasks/RansacLineFitting.h
+++ mbzirc2020/mbzirc2020_task1/mbzirc2020_task1_tasks/include/mbzirc2020_task1_tasks/RansacLineFitting.h
@@ -1,5 +1,6 @@
 #include <mbzirc2020_task1_tasks/GRANSAC.hpp>
 #include <mbzirc2020_task1_tasks/LineModel.hpp>
+#include <mbzirc2020_task1_tasks/Line3DModel.hpp>
 
 #include <cmath>
 #include <random>
@@ -25,12 +26,16 @@ private:
   ros::Publisher fitted_line_pub_;
   ros::Publisher fitted_points_pub_;
 
-  std::vector<std::shared_ptr<GRANSAC::AbstractParameter>> cand_points_;
-  int cand_points_max_size_;
+  std::vector<std::shared_ptr<GRANSAC::AbstractParameter>> cand_points2d_;
+  std::vector<std::shared_ptr<GRANSAC::AbstractParameter>> cand_points3d_;
+  int cand_points2d_max_size_;
+  int cand_points3d_max_size_;
+  GRANSAC::RANSAC<Line2DModel, 2> estimator_2d_;
+  GRANSAC::RANSAC<Line3DModel, 2> estimator_3d_;
   double lpf_z_;
   double lpf_z_gain_;
   bool ransac_vis_flag_;
-  GRANSAC::RANSAC<Line2DModel, 2> estimator_2d_;
+  bool ransac_3d_mode_;
 
   void targetPointCallback(const geometry_msgs::PointStampedConstPtr & msg);
   void visualizeRansacLine();
