ed42d1d9c31683ae653d43ba3194d630e2f5936b
fanshi14 <shifan@jsk.imi.i.u-tokyo.ac.jp>
2020-02-01 19:53:56 +0900

    Add experiment safety z offset to avoid crash.

--- mbzirc2020/mbzirc2020_task1/mbzirc2020_task1_tasks/src/ReactiveMotion.cpp
+++ mbzirc2020/mbzirc2020_task1/mbzirc2020_task1_tasks/src/ReactiveMotion.cpp
@@ -7,6 +7,7 @@ ReactiveMotion::ReactiveMotion(ros::NodeHandle nh, ros::NodeHandle nhp){
   nhp_.param("control_frequency", control_freq_, 100.0);
   nhp_.param("cog_net_offset", cog_net_offset_, 0.15);
   nhp_.param("target_pos_xy_threshold", target_pos_xy_thre_, 3.0);
+  nhp_.param("experiment_safety_z_offset", experiment_safety_z_offset_, 0.0);
 
   ransac_line_estimator_ = new RansacLineFitting(nh_, nhp_);
   motion_state_ = STILL;
@@ -107,6 +108,7 @@ void ReactiveMotion::sendControlCmd(Eigen::Vector3d target_pos){
   nav_msg.pos_z_nav_mode = nav_msg.POS_MODE;
   nav_msg.target_pos_z = cur_pos_(2) + delta_pos(2);
   nav_msg.target_pos_z += cog_net_offset_;
+  nav_msg.target_pos_z += experiment_safety_z_offset_;
   // todo: add psi
   // nav_msg.psi_nav_mode = nav_msg.POS_MODE;
   // nav_msg.target_psi = -math.pi / 2.0 # 0.0
