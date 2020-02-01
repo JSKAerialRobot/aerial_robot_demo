973f767be6dbcc96a9de52eff76df1643cdaf571
Tomoki Anzai <anzai@jsk.imi.i.u-tokyo.ac.jp>
2020-02-01 22:05:44 +0900

    [mbzirc2020_common] add GPS navigation mode in hydrus_interface

--- mbzirc2020/mbzirc2020_common/scripts/mbzirc2020_common/hydrus_interface.py
+++ mbzirc2020/mbzirc2020_common/scripts/mbzirc2020_common/hydrus_interface.py
@@ -1,5 +1,5 @@
 import rospy
-from sensor_msgs.msg import JointState
+from sensor_msgs.msg import JointState, NavSatFix
 from nav_msgs.msg import Odometry
 from aerial_robot_msgs.msg import FlightNav
 import numpy as np
@@ -10,6 +10,19 @@ import math
 from aerial_robot_model.srv import AddExtraModule, AddExtraModuleRequest
 from std_msgs.msg import UInt8
 from jsk_rviz_plugins.msg import OverlayText
+from spinal.msg import Gps
+
+def mDegLat(lat):
+    lat_rad = lat * np.pi / 180.0
+    return 111132.09 - 566.05 * np.cos(2.0 * lat_rad) + 1.20 * np.cos(4.0 * lat_rad) - 0.002 * np.cos(6.0 * lat_rad)
+
+def mDegLon(lat):
+    lat_rad = lat * np.pi / 180.0
+    return 111415.13 * np.cos(lat_rad) - 94.55 * np.cos(3.0 * lat_rad) - 0.12 * np.cos(5.0 * lat_rad)
+
+def gps2xy(orig_gps, dst_gps):
+    return mDegLat(orig_gps[0]) * (dst_gps[0] - orig_gps[0]), -mDegLon(orig_gps[0]) * (dst_gps[1] - orig_gps[1])
+
 
 class HydrusInterface:
     def __init__(self, debug_view = False):
@@ -23,6 +36,7 @@ class HydrusInterface:
         self.target_yaw_ = None
 
         self.xy_pos_offset_ = [0, 0]
+        self.gps_lat_lon_ = None
 
         #flight state
         self.ARM_OFF_STATE = 0
@@ -45,12 +59,19 @@ class HydrusInterface:
         self.force_landing_pub_ = rospy.Publisher('teleop_command/force_landing', Empty, queue_size = 1)
         self.add_extra_module_client_ = rospy.ServiceProxy('hydrusx/add_extra_module', AddExtraModule)
         self.flight_state_sub_ = rospy.Subscriber('flight_state', UInt8, self.flightStateCallback)
+        self.ros_gps_sub_ = rospy.Subscriber('fix', NavSatFix, self.rosGpsCallback)
+        self.gps_sub_ = rospy.Subscriber('gps', Gps, self.gpsCallback)
 
         if self.debug_view_:
             self.nav_debug_pub_ = rospy.Publisher('~nav_debug', OverlayText, queue_size = 1)
 
         self.joint_update_freq_ = rospy.get_param("~joint_update_freq", 20)
 
+    def rosGpsCallback(self, msg):
+        self.gps_lat_lon_ = [msg.latitude, msg.longitude]
+
+    def gpsCallback(self, msg):
+        self.gps_lat_lon_ = msg.location
 
     def jointStateCallback(self, msg):
         self.joint_state_ = msg
@@ -193,7 +214,7 @@ class HydrusInterface:
         nav_msg.pos_z_nav_mode = FlightNav.NO_NAVIGATION
         self.navigation(nav_msg)
 
-    def goPos(self, frame, target_xy, target_z, target_yaw):
+    def goPos(self, frame, target_xy, target_z, target_yaw, gps_mode=False):
         nav_msg = FlightNav()
         if frame == 'global':
             nav_msg.control_frame = nav_msg.WORLD_FRAME
@@ -207,11 +228,18 @@ class HydrusInterface:
 
         nav_msg.header.stamp = rospy.Time.now()
         nav_msg.target = FlightNav.BASELINK
-        nav_msg.pos_xy_nav_mode = FlightNav.POS_MODE
+        if gps_mode:
+            nav_msg.pos_xy_nav_mode = FlightNav.GPS_WAYPOINT_MODE
+        else:
+            nav_msg.pos_xy_nav_mode = FlightNav.POS_MODE
         nav_msg.pos_z_nav_mode = FlightNav.POS_MODE
         nav_msg.psi_nav_mode = FlightNav.POS_MODE
-        nav_msg.target_pos_x = target_xy[0] + self.xy_pos_offset_[0]
-        nav_msg.target_pos_y = target_xy[1] + self.xy_pos_offset_[1]
+        if gps_mode:
+            nav_msg.target_pos_x = target_xy[0]
+            nav_msg.target_pos_y = target_xy[1]
+        else:
+            nav_msg.target_pos_x = target_xy[0] + self.xy_pos_offset_[0]
+            nav_msg.target_pos_y = target_xy[1] + self.xy_pos_offset_[1]
         nav_msg.target_psi = target_yaw
         nav_msg.target_pos_z = target_z
 
@@ -221,19 +249,22 @@ class HydrusInterface:
         self.target_z_ = target_z
         self.target_yaw_ = target_yaw
 
-    def isConvergent(self, frame, target_xy, target_z, target_yaw, pos_conv_thresh, yaw_conv_thresh, vel_conv_thresh):
+    def isConvergent(self, frame, target_xy, target_z, target_yaw, gps_mode, pos_conv_thresh, yaw_conv_thresh, vel_conv_thresh):
         current_xy = self.getBaselinkPos()[0:2]
         current_z = self.getBaselinkPos()[2]
         current_yaw = self.getBaselinkRPY()[2]
         current_vel = self.getBaselinkLinearVel()
 
-        if frame == 'global':
-            delta_pos = np.array([target_xy[0] - current_xy[0], target_xy[1] - current_xy[1], target_z - current_z])
-        elif frame == 'local':
-            delta_pos = np.array([target_xy[0], target_xy[1], target_z - current_z])
+        if gps_mode:
+            delta_pos = gps2xy(self.gps_lat_lon_, target_xy)
         else:
-            rospy.logerr('invalid frame %s' % (frame))
-            return
+            if frame == 'global':
+                delta_pos = np.array([target_xy[0] - current_xy[0], target_xy[1] - current_xy[1], target_z - current_z])
+            elif frame == 'local':
+                delta_pos = np.array([target_xy[0], target_xy[1], target_z - current_z])
+            else:
+                rospy.logerr('invalid frame %s' % (frame))
+                return
 
         delta_yaw = target_yaw - current_yaw
         if delta_yaw > np.pi:
@@ -259,11 +290,11 @@ class HydrusInterface:
         else:
             return False
 
-    def goPosWaitConvergence(self, frame, target_xy, target_z, target_yaw, pos_conv_thresh = 0.1, yaw_conv_thresh = 0.1, vel_conv_thresh = 0.1, timeout = 30):
-        self.goPos(frame, target_xy, target_z, target_yaw)
+    def goPosWaitConvergence(self, frame, target_xy, target_z, target_yaw, gps_mode = False, pos_conv_thresh = 0.1, yaw_conv_thresh = 0.1, vel_conv_thresh = 0.1, timeout = 30):
+        self.goPos(frame, target_xy, target_z, target_yaw, gps_mode)
         start_time = rospy.get_time()
 
-        while not self.isConvergent(frame, target_xy, target_z, target_yaw, pos_conv_thresh, yaw_conv_thresh, vel_conv_thresh):
+        while not self.isConvergent(frame, target_xy, target_z, target_yaw, gps_mode, pos_conv_thresh, yaw_conv_thresh, vel_conv_thresh):
             elapsed_time = rospy.get_time() - start_time
             if elapsed_time > timeout:
                 return False
