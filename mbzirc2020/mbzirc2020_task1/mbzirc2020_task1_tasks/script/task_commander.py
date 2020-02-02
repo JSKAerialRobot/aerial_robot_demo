b6edc8b7c7f4b2b459b63b9cea10587bae402b19
fanshi14 <shifan@jsk.imi.i.u-tokyo.ac.jp>
2020-02-02 23:32:38 +0900

    Add task1_state_machine based on smach.

---
+++ mbzirc2020/mbzirc2020_task1/mbzirc2020_task1_tasks/script/task_commander.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+""" move hydrus around """
+
+import sys
+import time
+import rospy
+import math
+
+from std_msgs.msg import Empty
+from std_msgs.msg import Int8
+
+class TaskCommander():
+    def __init__(self, name="task_commander"):
+        # init node
+        # rospy.init_node(name)
+
+        # define publisher
+        self.reactive_motion_state_pub = rospy.Publisher("/reactive_motion/state", Int8, queue_size=1)
+
+        # constants
+        self.WAIT_TIME = 0.5
+
+    def prepare_estimation(self):
+        # send arm and takeoff
+        time.sleep(self.WAIT_TIME)
+        msg = Int8()
+        msg.data = 1
+        self.reactive_motion_state_pub.publish(msg)
+        time.sleep(self.WAIT_TIME)
+
