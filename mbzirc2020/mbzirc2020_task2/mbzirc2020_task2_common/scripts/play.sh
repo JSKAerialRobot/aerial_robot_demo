8c8e5ec5b56979d592e4139153a4c4baae67422b
Tomoki Anzai <anzai@jsk.imi.i.u-tokyo.ac.jp>
2020-02-10 19:18:30 +0900

    [mbzirc2020_task2_common] improve play.sh

--- mbzirc2020/mbzirc2020_task2/mbzirc2020_task2_common/scripts/play.sh
+++ mbzirc2020/mbzirc2020_task2/mbzirc2020_task2_common/scripts/play.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosbag play $1 --clock --pause /rectangle_detection_color/target_object_color:=/rectangle_detection_color/target_object_color_orig /rectangle_detection_depth/target_object_depth:=/rectangle_detection_depth/target_object_depth_orig
+rosbag play $@ --clock --pause /rectangle_detection_color/target_object_color:=/rectangle_detection_color/target_object_color_orig /rectangle_detection_depth/target_object_depth:=/rectangle_detection_depth/target_object_depth_orig
