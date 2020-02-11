43fb4073f6c1fe925dbbb04968fafc0607af3b1d
hydrus <shifan@jsk.imi.i.u-tokyo.ac.jp>
2020-02-11 22:48:31 +0900

    Add sendCmdDirectly to avoid net/safety offset.

--- mbzirc2020/mbzirc2020_task1/mbzirc2020_task1_tasks/include/mbzirc2020_task1_tasks/ReactiveMotion.h
+++ mbzirc2020/mbzirc2020_task1/mbzirc2020_task1_tasks/include/mbzirc2020_task1_tasks/ReactiveMotion.h
@@ -51,6 +51,7 @@ private:
   ros::Publisher task_track_flag_pub_;
 
   void sendControlCmd(Eigen::Vector3d target_pos);
+  void sendControlCmdDirectly(Eigen::Vector3d target_pos);
   bool isTargetPosInSearchRegion();
 
   void controlTimerCallback(const ros::TimerEvent& event);
