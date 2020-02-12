286e2bc459c54012070ea1b88d867dcc51631837
hydrus <shifan@jsk.imi.i.u-tokyo.ac.jp>
2020-02-12 20:59:29 +0900

    - set the pitch angle for monocular camera. - set the model for deep detection and tracking. - refactor the rosbag record script for task1

--- mbzirc2020/mbzirc2020_task1/mbzirc2020_task1_tasks/bin/rosbag_motion_record.sh
+++ mbzirc2020/mbzirc2020_task1/mbzirc2020_task1_tasks/bin/rosbag_motion_record.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rosrun aerial_robot_base rosbag_control_data.sh /realsense1/odom/throttle /realsense1/odom /rs_d435/color/image_rect_color /rs_d435/color/camera_info /usb_cam/image_raw /usb_cam/camera_info  /reactive_motion/state /reactive_motion/target /reactive_motion/cog_point /treasure/line_fitting_marker /treasure/point_detected /treasure/points_fitting_markers /ball_pos /drone_ball_detection_and_tracking/detection_result_image/compressed  --split --duration=30
+rosrun hydrus rosbag_control_data.sh /realsense1/odom/throttle /realsense1/odom /rs_d435/color/image_rect_color /rs_d435/color/camera_info /usb_cam/image_raw /usb_cam/camera_info  /reactive_motion/state /reactive_motion/target /reactive_motion/cog_point /treasure/line_fitting_marker /treasure/point_detected /treasure/points_fitting_markers /ball_pos /drone_ball_detection_and_tracking/detection_result_image/compressed  --split --duration=30
