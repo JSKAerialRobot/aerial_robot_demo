b46147f9115cc113f0f1e884b96b9fc82921588d
fanshi14 <shifan@jsk.imi.i.u-tokyo.ac.jp>
2020-02-19 18:27:40 +0900

    Open joints to release object while final landing process.

--- mbzirc2020/mbzirc2020_task1/mbzirc2020_task1_tasks/script/task1_land_state_machine.py
+++ mbzirc2020/mbzirc2020_task1/mbzirc2020_task1_tasks/script/task1_land_state_machine.py
@@ -47,7 +47,9 @@ def main():
         # smach.StateMachine.add('TASK1_ENTER_LAND_STATE', PrepareTaskState(), transitions={'success':'TASK1_ENTER_LAND_STATE_PREPARED', 'fail':'FAIL'})
 
         # smach.StateMachine.add('TASK1_ENTER_LAND_STATE_PREPARED', LandingState(15), transitions={'success':'DONE', 'fail':'INITIAL_STATE'})
-        smach.StateMachine.add('TASK1_ENTER_LAND_STATE', LandingState(15), transitions={'success':'DONE', 'fail':'INITIAL_STATE'})
+
+        # smach.StateMachine.add('TASK1_ENTER_LAND_STATE', LandingState(15), transitions={'success':'DONE', 'fail':'INITIAL_STATE'})
+        smach.StateMachine.add('TASK1_ENTER_LAND_STATE', LandingStateWithOpenJoints(15), transitions={'success':'DONE', 'fail':'INITIAL_STATE'})
         
 
     sis = smach_ros.IntrospectionServer('smach_server', sm, '/SM_ROOT')
