2f32a2058eb26d7af3a6df6c5bcf63e597c1848e
Tomoki Anzai <anzai@jsk.imi.i.u-tokyo.ac.jp>
2020-09-14 07:35:36 -0700

    [mbzirc2020_task2_common] improve basefootprint tf publiser

--- mbzirc2020/mbzirc2020_task2/mbzirc2020_task2_common/nodes/base_footprint_tf_publisher.py
+++ mbzirc2020/mbzirc2020_task2/mbzirc2020_task2_common/nodes/base_footprint_tf_publisher.py
@@ -2,7 +2,7 @@
 
 import rospy
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import TransformStamped, Transform
+from geometry_msgs.msg import TransformStamped, Transform, Quaternion
 import tf2_ros
 import tf.transformations as tft
 import ros_numpy
@@ -11,27 +11,26 @@ class BaseFootprintTfPublisher:
     def __init__(self):
         rospy.init_node('base_footprint_tf_publisher')
 
-        self.tf_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
-
         self.br = tf2_ros.TransformBroadcaster()
+        self.robot_ns = rospy.get_param('~robot_ns')
+        self.yaw_offset = rospy.get_param('~yaw_offset', 0.0)
+        self.odom_sub = rospy.Subscriber('uav/baselink/odom', Odometry, self.odomCallback)
 
-    def execute(self):
-        r = rospy.Rate(10)
-        while not rospy.is_shutdown():
-            try:
-                trans = self.tf_buffer.lookup_transform('world', 'fc', rospy.Time(), rospy.Duration(0.5))
-                trans.transform.translation.z = 0.0
-                trans.child_frame_id = "base_footprint"
-                self.br.sendTransform(trans)
+    def odomCallback(self, msg):
+        trans = TransformStamped()
+        trans.header = msg.header
+        trans.header.frame_id = '/world'
+        trans.child_frame_id = self.robot_ns + '/base_footprint'
+        trans.transform.translation.x = msg.pose.pose.position.x
+        trans.transform.translation.y = msg.pose.pose.position.y
+        trans.transform.translation.z = 0.0
 
-            except:
-                rospy.logerr("tf listen failed")
-
-            r.sleep()
-
+        euler_orig = tft.euler_from_quaternion(ros_numpy.numpify(msg.pose.pose.orientation))
+        euler = [0] * 3
+        euler[2] = euler_orig[2] + self.yaw_offset
+        trans.transform.rotation = ros_numpy.msgify(Quaternion, tft.quaternion_from_euler(*euler))
+        self.br.sendTransform(trans)
 
 if __name__ == '__main__':
     node = BaseFootprintTfPublisher()
-    node.execute()
     rospy.spin()
