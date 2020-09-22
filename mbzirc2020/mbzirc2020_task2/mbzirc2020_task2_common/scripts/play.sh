60a31f0cf8aede12839a7ccb0d2501cf512f0a24
Tomoki Anzai <anzai@jsk.imi.i.u-tokyo.ac.jp>
2020-09-22 05:11:11 -0700

    [mbzirc2020_task2_common] update debug files

--- mbzirc2020/mbzirc2020_task2/mbzirc2020_task2_common/scripts/play.sh
+++ mbzirc2020/mbzirc2020_task2/mbzirc2020_task2_common/scripts/play.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosbag play $@ --clock --pause /rectangle_detection_color/target_object_color:=/rectangle_detection_color/target_object_color_orig /rectangle_detection_depth/target_object_depth:=/rectangle_detection_depth/target_object_depth_orig
+rosbag play $@ --clock --pause /hydrus/rectangle_detection_color/target_object_color:=/hydrus/rectangle_detection_color/target_object_color_orig /hydrus/rectangle_detection_depth/target_object_depth:=/hydrus/rectangle_detection_depth/target_object_depth_orig
