62e469975055d750e49fd64644ae687d06fd1627
Tomoki Anzai <anzai@jsk.imi.i.u-tokyo.ac.jp>
2020-10-15 02:02:24 -0700

    [mbzirc2020_task2_common] update play.sh

--- mbzirc2020/mbzirc2020_task2/mbzirc2020_task2_common/scripts/play.sh
+++ mbzirc2020/mbzirc2020_task2/mbzirc2020_task2_common/scripts/play.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosbag play $@ --clock --pause /hydrus/rectangle_detection_color/target_object_color:=/hydrus/rectangle_detection_color/target_object_color_orig /hydrus/rectangle_detection_depth/target_object_depth:=/hydrus/rectangle_detection_depth/target_object_depth_orig
+rosbag play $@ --clock --pause /hydrus/rectangle_detection_color/target_object_color:=/hydrus/rectangle_detection_color/target_object_color_orig /hydrus/rectangle_detection_depth/target_object_depth:=/hydrus/rectangle_detection_depth/target_object_depth_orig hydrus/kf/alt1/data:=/hydrus/kf/alt1/data_orig hydrus/kf/gps1/data:=hydrus/kf/gps1/data_orig hydrus/kf/imu1/data:=hydrus/kf/imu1/data_orig hydrus/kf/vo1/data:=hydrus/kf/vo1/data_orig /hydrus/kf/plane_detection1/data:=/hydrus/kf/plane_detection1/data_orig
