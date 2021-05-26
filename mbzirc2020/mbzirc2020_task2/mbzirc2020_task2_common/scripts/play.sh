fb13032e28ca989d5b25fb2e550d2067797b3e5a
Tomoki Anzai <anzai@jsk.imi.i.u-tokyo.ac.jp>
2021-05-26 14:51:56 +0900

    [mbzirc2020_task2_common] add topic remap of channel_center/pos in play.sh

--- mbzirc2020/mbzirc2020_task2/mbzirc2020_task2_common/scripts/play.sh
+++ mbzirc2020/mbzirc2020_task2/mbzirc2020_task2_common/scripts/play.sh
@@ -8,4 +8,4 @@ echo namespace=$NS
 
 shift
 
-rosbag play $@ --clock --pause $NS/rectangle_detection_color/target_object_color:=$NS/rectangle_detection_color/target_object_color_orig $NS/rectangle_detection_depth/target_object_depth:=$NS/rectangle_detection_depth/target_object_depth_orig $NS/kf/alt1/data:=$NS/kf/alt1/data_orig $NS/kf/gps1/data:=$NS/kf/gps1/data_orig $NS/kf/imu1/data:=$NS/kf/imu1/data_orig $NS/kf/vo1/data:=$NS/kf/vo1/data_orig $NS/kf/plane_detection1/data:=$NS/kf/plane_detection1/data_orig
+rosbag play $@ --clock --pause $NS/rectangle_detection_color/target_object_color:=$NS/rectangle_detection_color/target_object_color_orig $NS/rectangle_detection_depth/target_object_depth:=$NS/rectangle_detection_depth/target_object_depth_orig $NS/channel_center/pos:=$NS/channel_center/pos_orig $NS/kf/alt1/data:=$NS/kf/alt1/data_orig $NS/kf/gps1/data:=$NS/kf/gps1/data_orig $NS/kf/imu1/data:=$NS/kf/imu1/data_orig $NS/kf/vo1/data:=$NS/kf/vo1/data_orig $NS/kf/plane_detection1/data:=$NS/kf/plane_detection1/data_orig
